6439eb2c62edb62f4b1817edd3ae500e977ca0fe
rvronski <rvronski@gmail.com>
2023-01-06 03:37:55 +0500

    Initial Commit

---
+++ MyPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlayer
+//
+//  Created by ROMAN VRONSKY on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
